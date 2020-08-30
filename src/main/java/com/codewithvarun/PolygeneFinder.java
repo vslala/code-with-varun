40c3547631e2adf7bd681a88b879645744d6461f
Varun shrivastava <varunshrivastava007@gmail.com>
2020-08-30 16:57:20 +0530

    optimize the hash code computation

--- src/main/java/com/codewithvarun/PolygeneFinder.java
+++ src/main/java/com/codewithvarun/PolygeneFinder.java
@@ -6,6 +6,10 @@ import java.nio.file.Paths;
 
 public class PolygeneFinder {
 
+    private int[] powersOf31 = {
+            1, 31, 961, 29791, 923521, 28629151, 887503681, 1742810335, -1807454463, -196513505, -1796951359, 129082719, -293403007, -505558625, 1507551809, -510534177, 1353309697, -997072353, -844471871, -408824225, 211350913, -2038056289, 1244764481, -67006753, -2077209343, 31019807, 961614017, -254736545, 693101697, 11316127, 350799937, -2010103841, 2111290369, 1025491999, 1725480897, 1950300255, 329765761, 1632803999, -922683583, 1461579999, -1935660287, 124073247, -448696639, -1024693921, -1700740479, -1183347297, 1970939457, 969581023, -7759359, -240540129, 1133190593, 769170015, -1925533311, 438009503, 693392705, 20337375, 630458625, -1930619105, 280349889, 100911967, -1166696319, -1807847521, -208698303, 2120287199
+    };
+
     public void printAll() {
         try {
             String[] words = Files.readAllLines(Paths.get("/usr/share/dict/words")).toArray(new String[]{});
@@ -18,8 +22,9 @@ public class PolygeneFinder {
     private void analyzeWordPair(String[] words) {
         for (String left : words) {
             for (String right : words) {
+                int hash = left.hashCode() * powersOf31[right.length()] + right.hashCode();
                 String pair = left + right;
-                if (pair.hashCode() == Integer.MIN_VALUE) {
+                if (hash == Integer.MIN_VALUE) {
                     System.out.println(pair);
                 }
             }
--- src/test/java/com/codewithvarun/PolygeneFinderTest.java
+++ src/test/java/com/codewithvarun/PolygeneFinderTest.java
@@ -2,6 +2,10 @@ package com.codewithvarun;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.joining;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PolygeneFinderTest {
@@ -10,6 +14,12 @@ class PolygeneFinderTest {
     public void itShouldPrintAllThePolygeneWords() {
         var polygeneFinder = new PolygeneFinder();
         polygeneFinder.printAll();
+    }
+
+    @Test
+    public void sandbox() {
+        String powersOf31 = IntStream.iterate(1, x -> x * 31).limit(64).mapToObj(String::valueOf).collect(joining(", "));
+        System.out.println(powersOf31);
     }
 
 }
