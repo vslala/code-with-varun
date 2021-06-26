5f52082f4888780d7fa4796e8bcc6b4145c3b2f9
Varun <varun.shrivastava@klarna.com>
2021-06-26 21:15:21 +0200

    Varun: cleanup

--- src/main/java/com/codewithvarun/sort/RuntimeAnalyser.java
+++ src/main/java/com/codewithvarun/sort/RuntimeAnalyser.java
@@ -1,6 +1,5 @@
 package com.codewithvarun.sort;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
--- src/test/java/com/codewithvarun/sort/BottomUpMergeSortTest.java
+++ src/test/java/com/codewithvarun/sort/BottomUpMergeSortTest.java
@@ -8,7 +8,8 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BottomUpMergeSortTest {
     private Sort selectionSort;
